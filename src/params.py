8b785a244f754268a0440933e75564a91df1ebd3
claraElk <clara.elkhantour@gail.com>
2022-10-27 19:38:05 -0400

    Add src folder with params

---
+++ src/params.py
@@ -0,0 +1,4 @@
+DISK_PATH = '/run/media/claraek/Clara_Seagate/SoLaugh_MSc/0_RAW_DATA/'
+SUBJECTS = [str(i).zfill(2) for i in range(0, 33)]
+RUNS = [str(i).zfill(2) for i in range(1, 13)]
+BIDS_PATH = '/run/media/claraek/Clara_Seagate/SoLaugh_MSc/BIDS_DATA/'
