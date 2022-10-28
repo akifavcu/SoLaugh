932d362bc7f6c7e745f883f27d5b6b79847a2689
claraElk <clara.elkhantour@gail.com>
2022-10-27 21:38:35 -0400

    generate BIDS draft

--- data/mne_bids.py
+++ data/mne_bids.py
@@ -1,15 +1,17 @@
 import mne
 import os
 from mne_bids import BIDSPath, write_raw_bids
-from params import DISK_PATH, BIDS_PATH, SUBJECTS, RUNS
+from params import DISK_PATH, BIDS_PATH, SUBJECTS, RUNS, EVENTS_ID
 
 
 # List of the directory for each participant
-folder_data = os.listdir(DISK_PATH) 
+folder_subj = os.listdir(DISK_PATH) 
 # Remove non subject file
-folder_data.remove('Cahier_manip_Laughter_MEG.pdf')
-folder_data.remove('S28.zip')
+folder_subj.remove('Cahier_manip_Laughter_MEG.pdf')
+folder_subj.remove('S28.zip')
 
+passive_run = RUNS[1:6] # Select runs 02 to 05
+active_run = RUNS[7:13] # Select runs 07 to 12
 
 # check if BIDS_PATH exists, if not, create it
 if not os.path.isdir(BIDS_PATH):
@@ -19,28 +21,81 @@ else:
     print("{} already exists.".format(BIDS_PATH))
 
 
-for old_file in folder_data :
+for old_file in folder_subj : # Go through S1, S2 etc...
 
-    old_path = os.path.join(disk_path, folder_data)
-    file_list = os.listdir(old_path)
-    
-    for subj in SUBJECTS:
-        for run in RUNS:
+    old_path = os.path.join(disk_path, old_file)
+    file_list = os.listdir(old_path) 
     
     # For NOISE file        
-    for files in file_list :
-        if 'NOISE' in files :
-    
+    for CAfile in file_list : # Go through CA01_Laughter ...
+        subj_number = folder_subj[1:] # Remove the letter S
+        if len(subj) == 1 :
+            subj = '0' + subj_number
+
+        if 'NOISE_noise_' in CAfile :
             noise_bids_path = BIDSPath(
                 subject=subj,
-                session='01' 
-                run=run,
                 task='NOISE'
                 datatype='meg',
                 extension='.ds' 
                 root=BIDS_PATH)
 
-            write_raw_bids(raw, bids_path=noise_bids_path)
-    
-    # For Task file
+            # Convert NOISE files into BIDS
+            raw_fname = os.path.join(oldpath, CAfile)
+            raw = mne.io.read.cft(raw_fname)
+            write_raw_bids(raw, bids_path=noise_bids_path, overwrite = True)
+   
+        # For Task file
+        if 'Laughter' in CAfile :
+
+            subj = CAfile[2:4]
+            run = CAfile[-5:-3]
+
+            if run == '01' or run == '06' :
+                task = 'RS'
+            elif run in passive_run :
+                task = 'passive'
+            elif run in active_run :
+                task = 'active'
+            
+            laughter_bids_path = BIDSPath(                
+                subject=subj, 
+                run = run,
+                task=task
+                datatype='meg',
+                extension='.ds' 
+                root=BIDS_PATH) 
+                
+            # Convert laughter files into BIDS 
+            raw_fname = os.path.join(oldpath, CAfile)
+            raw = mne.io.read.cft(raw_fname)
+
+            if task == 'active' or task == 'passive':
+                events = mne.find_events(raw)
+
+                write_raw_bids(
+                    raw, 
+                    bids_path=laughter_bids_path, 
+                    events_data = events,
+                    event_id = EVENTS_ID,
+                    overwrite = True
+                    )
+            else :
+                write_raw_bids(raw, bids_path=laughter_bids_path)
+
+        # For procedure file
+        if 'procedure' in CAfile : 
+            subj = CAfile[2:4]
+            
+            procedure_bids_path = BIDSPath(                
+                subject=subj,
+                task='procedure'
+                datatype='meg',
+                extension='.ds' 
+                root=BIDS_PATH) 
+
+            # Convert procedure file into BIDS
+            raw_fname = os.path.join(oldpath, CAfile)
+            raw = mne.io.read.cft(raw_fname)  
+            write_raw_bids(raw, bids_path=procedure_bids_path, overwrite = True)
 
