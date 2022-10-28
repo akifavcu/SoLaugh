227db855548c7f3d8fe0f0e6526355939304f18f
claraElk <clara.elkhantour@gail.com>
2022-10-27 21:39:50 -0400

    Add Events ID

--- src/params.py
+++ src/params.py
@@ -1,4 +1,8 @@
-DISK_PATH = '/run/media/claraek/Clara_Seagate/SoLaugh_MSc/0_RAW_DATA/'
-SUBJECTS = [str(i).zfill(2) for i in range(0, 33)]
-RUNS = [str(i).zfill(2) for i in range(1, 13)]
+# Paths 
+DISK_PATH = '/run/media/claraek/Clara_Seagate/SoLaugh_AL/0_RAW_DATA/'
 BIDS_PATH = '/run/media/claraek/Clara_Seagate/SoLaugh_MSc/BIDS_DATA/'
+
+# Task parameters
+SUBJECTS = [str(i).zfill(2) for i in range(0, 33)] # Need to remove participant 17
+RUNS = [str(i).zfill(2) for i in range(1, 13)]
+EVENTS_ID = {'LaughReal' : 11, 'LaughPosed' : 12, 'Hit' : 99, 'False' : 66, 'Offset' : 5}
