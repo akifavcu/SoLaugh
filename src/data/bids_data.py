1ec500083cd5c2a8da99339503e4a2c94e2f0a42
claraElk <clara.elkhantour@gail.com>
2022-11-02 09:53:56 -0400

    Add some comments

--- src/data/bids_data.py
+++ src/data/bids_data.py
@@ -2,15 +2,15 @@ import mne
 import os
 from mne_bids import BIDSPath, write_raw_bids
 from src.params import DISK_PATH, BIDS_PATH, EVENTS_ID, RUNS
-# Pb with the sys.path
 
 # List of the directory for each participant
 folder_subj = os.listdir(DISK_PATH) 
+
 # Remove non subject files
 folder_subj.remove('Cahier_manip_Laughter_MEG.pdf')
 folder_subj.remove('S28.zip')
 folder_subj.remove('S0_Pilote')
-folder_subj.remove('S17_removed') #subject remove from the task 
+folder_subj.remove('S17_removed') #S17 did not finish task
 
 passive_run = RUNS[1:5] # Select runs 02 to 05
 active_run = RUNS[6:13] # Select runs 07 to 12
@@ -22,18 +22,21 @@ if not os.path.isdir(BIDS_PATH):
 else:
     print("{} already exists.".format(BIDS_PATH))
 
-
-for old_file in folder_subj : # Go through S1, S2 etc...
+# Go through all participants files (S1, S2...)
+for old_file in folder_subj : 
 
     old_path = os.path.join(DISK_PATH, old_file)
     file_list = os.listdir(old_path) 
     
-    for CAfile in file_list : # Go through CA01_Laughter ...
+    #Go through all run and noise files
+    for CAfile in file_list :
 
         # For NOISE file 
         if 'NOISE_noise_' in CAfile and not '.fif' in CAfile:
 
             subj_number = old_file[1:] # Remove the letter S
+            
+            #Subject name must be in format '01'
             if len(subj_number) == 1 :
                 subj = '0' + subj_number
             else :
@@ -53,12 +56,13 @@ for old_file in folder_subj : # Go through S1, S2 etc...
             noise_raw = mne.io.read_raw_ctf(noise_raw_fname)
             write_raw_bids(noise_raw, bids_path=noise_bids_path, overwrite = True)
    
-        # For Task file
+        # For Task files
         if '_Laughter-' and '.ds' in CAfile and not '.zip' in CAfile and not 'NOISE' in CAfile and not 'procedure' in CAfile:
 
             subj = CAfile[2:4]
             run = CAfile[-5:-3]
 
+            # Associate run with task
             if run == '01' or run == '06' :
                 task = 'RS'
             elif run in passive_run :
@@ -80,6 +84,7 @@ for old_file in folder_subj : # Go through S1, S2 etc...
             raw_fname = os.path.join(old_path, CAfile)
             raw = mne.io.read_raw_ctf(raw_fname)
 
+            # Find events in file
             if task == 'LaughterActive' or task == 'LaughterPassive':
                 events = mne.find_events(raw, initial_event=False)
 
@@ -93,6 +98,7 @@ for old_file in folder_subj : # Go through S1, S2 etc...
             else :
                 write_raw_bids(raw, bids_path=laughter_bids_path, overwrite = True)
         
+        # We don't want to convert all other NOISE files
         if 'NOISE' in CAfile and 'Trial' in CAfile :
             continue
         
