0cd44e64e8b7b0e44e7652994f809bc6a46e1260
claraElk <clara.elkhantour@gail.com>
2022-11-14 21:40:14 -0500

    Add functions

--- src/behavior/discrimination_acc.py
+++ src/behavior/discrimination_acc.py
@@ -2,123 +2,58 @@ import mne
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
-from src.params import BIDS_PATH, SUBJ_LIST, ACTIVE_RUN, RESULT_PATH
+from src.params import PERF_PATH, BEHAV_PATH
 
-# check if RESULT_PATH/behavior/behavior exists, if not, create it
-BEHAV_PATH = RESULT_PATH + 'DISCRIMINATION/behavior/'
+def all_perf_data(perf_path) :
 
-if not os.path.isdir(BEHAV_PATH):
-    os.mkdir(BEHAV_PATH)
-    print("BEHAV folder created at : {}".format(BEHAV_PATH))
-else:
-    print("{} already exists.".format(BEHAV_PATH))
+    '''TODO'''
 
-# Initiate all list
-df_results = pd.DataFrame()
-subj_loop = []
-acc_list = []
-hit_false = []
-score = []
-laugh_type = []
-nb_resp = []
-laughter_predict = []
+    df_data = pd.DataFrame()
+    for subj_perf in os.listdir(perf_path) :
+        if '.csv' in subj_perf : 
+            df_subj = pd.read_csv(perf_path + subj_perf)
+            df_data = pd.concat([df_data, df_subj])
+    df_data['nb_resp'] = 1   
 
-for subj in SUBJ_LIST:
+    # Export table into csv
+    df_data.to_csv(BEHAV_PATH + 'discrimination_performance.csv')
+    return df_data
 
-    for run in ACTIVE_RUN:
+def plot_perf(df) : 
 
-        # Read raw file
-        filename = 'sub-{}/ses-recording/meg/sub-{}_ses-recording_task-LaughterActive_run-{}_meg.ds'.format(subj, subj, run)
-        fname = os.path.join(BIDS_PATH, filename)
-        raw = mne.io.read_raw_ctf(fname)
+    '''TODO'''
 
-        # Find events
-        events = mne.find_events(raw, initial_event=False)
-        events_corrected = list(events[:, [2]])
+    # TODO : Change variables to plot 
+    # Plot differences between hit and false
+    grouped_hit_false = df.groupby(['subID', 'response', 'active_laughType']).count()
+    grouped_hit_false.boxplot(column = 'nb_resp', by = ['response'])
+    plt.ylabel('Number of answer')
+    plt.title('Overall performance')
+    plt.savefig(BEHAV_PATH + 'discrimination_performance-hit-false.png')
+    plt.show()
 
-        # Interpolate missing answers
-        for i, ev in enumerate(events_corrected) :
-            if i == (len(events_corrected)-1) and ev == 5 :
-                events_corrected.insert(i+1, [66])
-            elif ev == 5 and (events_corrected[i+1] == 11 or events_corrected[i+1] == 12): 
-                # Add False where response is missing
-                events_corrected.insert(i+1, [66])
+    # Plot differences between laughter type performance
+    grouped_real_posed = df.groupby(['subID', 'response', 'active_laughType']).count()
+    grouped_real_posed.boxplot(column = 'nb_resp', by = ['response', 'active_laughType'])
+    plt.ylabel('Number of answer')
+    plt.title('Performance for each type of laughter')
+    plt.savefig(BEHAV_PATH + 'discrimination_performance-real-posed.png')
+    plt.show()
 
-        events_array = np.array(events_corrected)
+    return grouped_hit_false, grouped_real_posed
 
-        # Compute real and posed laugh
-        for stim in events_array :
-            if stim == 11:
-                laugh_type.append('Real')
-            elif stim == 12 :
-                laugh_type.append('Posed')
+if __name__ == '__main__' :
 
-        # Compute Hit and False answers
-        for ev in events_array :
-            if ev == 99 :
-                hit_false.append('Hit')
-                subj_loop.append(subj)
-                score.append(1)
-                
-            elif ev == 66 :
-                hit_false.append('False')
-                subj_loop.append(subj)
-                score.append(0)
+    # check if the path exists, if not, create it
+    if not os.path.isdir(BEHAV_PATH):
+        os.mkdir(BEHAV_PATH)
+        print('BEHAV folder created at : {}'.format(BEHAV_PATH))
+    else:
+        print('{} already exists.'.format(BEHAV_PATH))
 
-        # TODO : compute reaction time -> events[:, [0]]
-        # Compute reaction time
+    df_data = all_perf_data(PERF_PATH)
 
-#Compute the number of response
-for resp in hit_false :
-    nb_resp.append(1)
+    grouped_hit_false, grouped_real_posed = plot_perf(df_data)
 
-# Implement a multi-index table
-df = pd.DataFrame(data = {'Subject': subj_loop, 'Hit/False': hit_false, 'Score': score,
-'LaughType': laugh_type, 'NumberResponse': nb_resp})
 
-# Export table into csv
-df.to_csv(RESULT_PATH + 'discrimination_performance.csv')
-
-# Plot graph showing differences between hit and false
-grouped_hit_false = df.groupby(['Subject', 'Hit/False', 'LaughType']).count()
-grouped_hit_false.boxplot(column = 'NumberResponse', by = ['Hit/False'])
-plt.ylabel('Number of answer')
-plt.title('Overall performance')
-plt.savefig(BEHAV_PATH + 'discrimination_performance-hit-false.png')
-plt.show()
-
-# Plot graph showing differences between laughter type performance
-grouped_real_posed = df.groupby(['Subject', 'Hit/False', 'LaughType']).count()
-grouped_real_posed.boxplot(column = 'NumberResponse', by = ['Hit/False', 'LaughType'])
-plt.ylabel('Number of answer')
-plt.title('Performance for each type of laughter')
-plt.savefig(BEHAV_PATH + 'discrimination_performance-real-posed.png')
-plt.show()
-
-# Plot a confusion matrix
-for i, resp in enumerate(df['Score']) :
-    if resp == 0 :
-        if df['LaughType'][i] == 'Real' :
-            laughter_predict.append('Posed')
-        elif df['LaughType'][i] == 'Posed' :
-            laughter_predict.append('Real')
-    elif resp == 1 :
-        if df['LaughType'][i] == 'Real' :
-            laughter_predict.append('Real')
-        elif df['LaughType'][i] == 'Posed' :
-            laughter_predict.append('Posed')
-
-df['LaughPredict'] = laughter_predict
-
-df['y_actual'] = df['LaughType'].map({'Real': 1, 'Posed': 0})
-df['y_predicted'] = df['LaughPredict'].map({'Real': 1, 'Posed': 0})
-
-confusion_matrix = pd.crosstab(df['y_actual'], df['y_predicted'], rownames=['Actual'], 
-                    colnames=['Predicted'])
-
-print(confusion_matrix)
-sns.heatmap(confusion_matrix, annot=True)
-plt.savefig(BEHAV_PATH + 'confusion_matrix_real-posed.png')
-plt.show()
