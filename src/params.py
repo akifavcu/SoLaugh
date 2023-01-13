bcbc2ce358137a97d56b1d857799ef86f18a825d
claraElk <clara.elkhantour@gmail.com>
2023-01-13 15:44:41 -0500

    Test push

--- src/params.py
+++ src/params.py
@@ -1,10 +1,10 @@
 # Paths 
-DISK_PATH = '/run/media/claraek/Clara_Seagate/SoLaugh_AL/0_RAW_DATA/'
-BIDS_PATH = '/run/media/claraek/Clara_Seagate/SoLaugh_MSc/BIDS_DATA/'
-RESULT_PATH = '/run/media/claraek/Clara_Seagate/SoLaugh_MSc/RESULTS/'
-PREPROC_PATH = '/run/media/claraek/Clara_Seagate/SoLaugh_MSc/PREPROC/'
-PERF_PATH = '/run/media/claraek/Clara_Seagate/SoLaugh_MSc/PERF/solaugh_task/results_active/'
-BEHAV_PATH = RESULT_PATH + 'DISCRIMINATION/behavior/'
+DISK_PATH = '/run/media/clara_elk/Clara_Seagate/SoLaugh_AL/0_RAW_DATA/'
+BIDS_PATH = '/run/media/clara_elk/Clara_Seagate/SoLaugh_MSc/bids_data/'
+RESULT_PATH = '/run/media/clara_elk/Clara_Seagate/SoLaugh_MSc/results/'
+PREPROC_PATH = '/run/media/clara_elk/Clara_Seagate/SoLaugh_MSc/preproc/'
+PERF_PATH = '/run/media/clara_elk/Clara_Seagate/SoLaugh_MSc/PERF/solaugh_task/results_active/'
+BEHAV_PATH = RESULT_PATH + 'discrimination/behavior/'
 
 # Task parameters
 SUBJ_LIST = [str(i).zfill(2) for i in range(1, 33)] 
---
+++ src/utils.py
@@ -0,0 +1,48 @@
+import os
+from src.params import BIDS_PATH, ACTIVE_RUN, PASSIVE_RUN, PREPROC_PATH
+
+def bids_files(BIDS_PATH, subj, run, stage) :
+    # Step 1 : get all parameters of the file name : subj, session, task
+    # Step 2 : stage gives us the extension and at what step in the processing we are => extension file
+    # Step 3 : put all the information in var laughter_bidsname
+    # Step 4 : Return laughter_bidspath
+
+    # stages : ave, epo, ica, cov, ica_epo, filt_raw, clean epo => .fif
+    # other stages : report (html)
+
+    # Find active and passive runs
+    if run in ACTIVE_RUN :
+        task = "LaughterActive"
+    elif run in PASSIVE_RUN :
+        task = "LaughterPassive"
+
+    # Find the extension of the file
+
+
+    # Raw data 
+    if  stage == "raw":
+        laughter_bidsname = "sub-{}_ses-recording_task-{}_run-{}_meg.ds".format(subj, task, run)
+        laughter_bidspath = os.path.join(BIDS_PATH, "sub-{}".format(subj), 
+        "ses-recording", "meg", laughter_bidsname)
+
+    # For preprocessed data 
+    elif (stage == "ave"  
+    or stage == "epo" 
+    or stage == "ica"
+    or stage == "cov"  
+    or stage == "ica_epo"
+    or stage == "clean epo") : 
+        
+        extension = ".fif"
+
+        laughter_bidsname = "sub-{}_ses-recording_task-{}_{}{}".format(subj, task, stage, extension)
+        laughter_bidspath = os.path.join(PREPROC_PATH, "sub-{}".format(subj), 
+        "ses-recording", "meg", laughter_bidsname)
+    
+    elif stage == "filt_raw" :
+        extension = ".fif"
+        laughter_bidsname = "sub-{}_ses-recording_task-{}_run-{}_{}{}".format(subj, task, run, stage, extension)
+        laughter_bidspath = os.path.join(PREPROC_PATH, "sub-{}".format(subj), 
+        "ses-recording", "meg", laughter_bidsname)
+    # For analysis
+    return laughter_bidspath
