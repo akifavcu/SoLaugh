6ed1d55e5c454036553dc537880c552e783cbdc3
Clara El Khantour <claraelk@narval3.narval.calcul.quebec>
2023-01-17 16:57:28 -0500

    Change slurm parameters

--- slurm/1_preprocessing.sh
+++ slurm/1_preprocessing.sh
@@ -4,6 +4,6 @@
 #SBATCH --job-name=SoLaughPreproc
 #SBATCH --mem=32G
 
-module load python/3.10.0
+module load python/3.10
 
-$HOME/laughter_meg/bin/mne-bids-pipeline $HOME/projects/def-kjerbi/claraelk/SoLaugh/src/config.py
+$HOME/laughter_meg/bin/mne_bids_pipeline $HOME/projects/def-kjerbi/claraelk/SoLaugh/src/data/preproc_config.py --subject=$1
