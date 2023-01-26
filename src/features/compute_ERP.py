1e776e78c284217921e7e88d1c9617fbd7305365
claraElk <clara.elkhantour@gmail.com>
2023-01-25 19:07:25 -0500

    Add pickle and start PSD

--- src/features/compute_ERP.py
+++ src/features/compute_ERP.py
@@ -3,13 +3,10 @@ import os
 import scipy.stats
 import matplotlib.pyplot as plt
 import numpy as np
+import pickle
 from src.utils import get_bids_file
-from src.params import BIDS_PATH, PREPROC_PATH, SUBJ_LIST, ACTIVE_RUN, FIG_PATH
-from mpl_toolkits.axes_grid1 import make_axes_locatable
+from src.params import BIDS_PATH, PREPROC_PATH, SUBJ_LIST, ACTIVE_RUN, RESULT_PATH
 from mne.stats import spatio_temporal_cluster_test, combine_adjacency
-from mne.datasets import sample
-from mne.channels import find_ch_adjacency
-from mne.viz import plot_compare_evokeds
 
 # Will be moved in another file later
 def ERP(PREPROC_PATH, subj_list, run_list, cond1, cond2, stage) :
@@ -28,23 +25,27 @@ def ERP(PREPROC_PATH, subj_list, run_list, cond1, cond2, stage) :
 
             epochs_concat = mne.concatenate_epochs([epochs]) # See if problem with head location
 
+    # Need to save epochs_concat
+    if len(subj_list) == 32 :
+        subj_name = "all"
+    else :
+        subj_name = "notall"
+
+    # TODO : Need to put that in params
+    save_cond1 = RESULT_PATH + "meg/reports/epochs/ave-epo_cond-{}_epochs.pkl".format(cond1)
+    save_cond2 = RESULT_PATH + "meg/reports/epochs/ave-epo_cond-{}_epochs.pkl".format(cond2)
+    save_concat = RESULT_PATH + "meg/reports/epochs/concat-epo_cond-{}-{}_epochs.pkl".format(cond1, cond2)
+
+    with open(save_cond1, 'wb') as f:
+        pickle.dump(condition1, f)
+
+    with open(save_cond2, 'wb') as f:
+        pickle.dump(condition2, f)
+    
+    with open(save_concat, 'wb') as f:
+        pickle.dump(epochs_concat, f)    
+
     return condition1, condition2, epochs_concat
-
-def plot_ERP(condition1, condition2, cond1_name, cond2_name, picks) :
-    # Plot each condition separately
-    fig_cond1 = condition1.plot_joint(picks = picks)
-    fname_cond1 = FIG_PATH + "plot-join_cond-" + cond1_name
-    fig_cond1.savefig(fname_cond1)
-
-    fig_cond2 = condition2.plot_joint(picks = picks)
-    fname_cond2 = FIG_PATH + "plot-join_cond-" + cond2_name
-    fig_cond2.savefig(fname_cond2)
-
-    # Plot ERPs LaughReal vs LaughPosed
-    evoked = dict(real = condition1, posed = condition2)
-    fig_ERP = mne.viz.plot_compare_evokeds(evoked, picks = picks, combine='mean')
-    fname_ERP = FIG_PATH + "ERP_cond1-" + cond1_name + "_cond2-" + cond2_name
-    # fig_ERP.savefig(fname_ERP) # Doesn't work
 
 def cluster_ERP(epochs, event_id) :
 
@@ -84,82 +85,21 @@ def cluster_ERP(epochs, event_id) :
                                                 adjacency=None)
     F_obs, clusters, p_values, _ = cluster_stats
 
-    return F_obs, clusters, p_values
+    # TODO : Need to find a better way to do this save !
+    save_Fobs = RESULT_PATH + "meg/reports/epochs/ERP-cluster_mes-Fobs_pval-{}.pkl".format(alpha_cluster_forming)
+    save_clusters = RESULT_PATH + "meg/reports/epochs/ERP-cluster_mes-clusters_pval-{}.pkl".format(alpha_cluster_forming)
+    save_pval = RESULT_PATH + "meg/reports/epochs/ERP-cluster_mes-pva_pval-{}.pkl".format(alpha_cluster_forming)
 
-def visualize_cluster(epochs, F_obs, clusters, p_values, event_id) :
+    with open(save_Fobs, 'wb') as f:
+        pickle.dump(F_obs, f)
 
-    # Code adapted from :
-    # https://mne.tools/stable/auto_tutorials/stats-sensor-space/75_cluster_ftest_spatiotemporal.html
+    with open(save_clusters, 'wb') as f:
+        pickle.dump(clusters, f)
     
-    p_accept = 0.01
-    good_cluster_inds = np.where(p_values < p_accept)[0]
-
-    # configure variables for visualization
-    colors = {"LaughReal": "crimson", "LaughPosed": 'steelblue'}
-
-    # organize data for plotting
-    evokeds = {cond: epochs[cond].average() for cond in event_id}
-
-    # loop over clusters
-    for i_clu, clu_idx in enumerate(good_cluster_inds):
-        # unpack cluster information, get unique indices
-        time_inds, space_inds = np.squeeze(clusters[clu_idx])
-        ch_inds = np.unique(space_inds)
-        print(ch_inds)
-        time_inds = np.unique(time_inds)
-
-        # get topography for F stat
-        f_map = F_obs[time_inds, ...].mean(axis=0)
-
-        # get signals at the sensors contributing to the cluster
-        sig_times = epochs.times[time_inds]
-
-        # create spatial mask
-        mask = np.zeros((f_map.shape[0], 1), dtype=bool)
-        mask[ch_inds, :] = True
-
-        # initialize figure
-        fig, ax_topo = plt.subplots(1, 1, figsize=(10, 3))
-
-        # plot average test statistic and mark significant sensors
-        f_evoked = mne.EvokedArray(f_map[:, np.newaxis], epochs.info, tmin=0)
-        f_evoked.plot_topomap(times=0, mask=mask, axes=ax_topo, cmap='Reds',
-                            vlim=(np.min, np.max), show=False,
-                            colorbar=False, mask_params=dict(markersize=10))
-        image = ax_topo.images[0]
-
-        # remove the title that would otherwise say "0.000 s"
-        ax_topo.set_title("")
-
-        # create additional axes (for ERF and colorbar)
-        divider = make_axes_locatable(ax_topo)
-
-        # add axes for colorbar
-        ax_colorbar = divider.append_axes('right', size='5%', pad=0.05)
-        plt.colorbar(image, cax=ax_colorbar)
-        ax_topo.set_xlabel(
-            'Averaged F-map ({:0.3f} - {:0.3f} s)'.format(*sig_times[[0, -1]]))
-
-        # add new axis for time courses and plot time courses
-        ax_signals = divider.append_axes('right', size='300%', pad=1.2)
-        title = 'Cluster #{0}, {1} sensor'.format(i_clu + 1, len(ch_inds))
-        if len(ch_inds) > 1:
-            title += "s (mean)"
-        plot_compare_evokeds(evokeds, title=title, picks=ch_inds, axes=ax_signals,
-                            colors=colors, linestyles=None, show=False,
-                            split_legend=True, truncate_yaxis='auto')
-        # plot temporal cluster extent
-        ymin, ymax = ax_signals.get_ylim()
-        ax_signals.fill_betweenx((ymin, ymax), sig_times[0], sig_times[-1],
-                                color='orange', alpha=0.3)
-
-        # clean up viz
-        mne.viz.tight_layout(fig=fig)
-        fig.subplots_adjust(bottom=.05)
-        fig.savefig(FIG_PATH + "plot-clusters_ERP")
-        plt.show()
-        "Done Visualize cluster"
-        return fig
+    with open(save_pval, 'wb') as f:
+        pickle.dump(p_values, f)
+        
+    return F_obs, clusters, p_values
 
 if __name__ == "__main__" :
     
@@ -177,11 +117,5 @@ if __name__ == "__main__" :
     # Compute ERPs
     condition1, condition2, epochs_concat = ERP(PREPROC_PATH, subj_list, run_list, cond1, cond2, "epo")
 
-    # Plot ERPs
-    plot_ERP(condition1, condition2, cond1, cond2, picks)
-
     # Compute ERP clusters
     F_obs, clusters, p_values = cluster_ERP(epochs_concat, event_id)
-
-    # Visualization of ERP clusters
-    fig = visualize_cluster(epochs_concat, F_obs, clusters, p_values, event_id)
--- src/features/compute_PSD.py
+++ src/features/compute_PSD.py
@@ -1,2 +1,71 @@
 import mne 
 from src.utils import get_bids_file
+from src.params import PREPROC_PATH
+
+def ave_epochs(PREPROC_PATH, subj_list, run_list, cond1, cond2, stage) :
+    
+    epochs_concat = []
+
+    for subj in subj_list :
+        for run in run_list :
+            epochs_file = get_bids_file(PREPROC_PATH, subj, run, stage)
+            epochs = mne.read_epochs(epochs_file)
+            print(epochs.info)
+
+            # Average each condition
+            epochs_cond1 = epochs[cond1].average()
+            epochs_cond2 = epochs[cond2].average()
+
+            epochs_concat = mne.concatenate_epochs([epochs]) # TODO : See if problem with head location
+
+    return epochs_cond1, epochs_cond2, epochs_concat
+
+#def compute_PSD(epochs_cond1, epochs_cond2, epochs_concat) :
+    # See https://natmeg.se/mne_timefrequency/MNE_timefrequency.html
+    # Compute epoch psd for each frequency
+    # Save each epoch psd file in h5 format for each frequency
+
+    # For each frequency :
+    #frequencies = np.arange(5., 120., 2.5)  # define frequencies of interest
+    #n_cycles = frequencies / 5.  # different number of cycle per frequency
+    #Fs = raw.info['sfreq']  # sampling in Hz
+    #decim = 5
+
+    #data = epochs_stim.get_data()
+    #from mne.time_frequency import induced_power
+    #power, phase_lock = induced_power(data, Fs=Fs, frequencies=frequencies,
+    #                              n_cycles=n_cycles, n_jobs=4, use_fft=False,
+    #                              decim=decim, zero_mean=True)
+    #power.shape, data.shape, frequencies.shape
+    # Save 
+
+def plot_PSD(epochs_cond1, epochs_cond2, epochs_concat) :
+    
+    # Plot PSD for condition 1
+    fig_cond1_psd = epochs_cond1.plot_psd_topomap(ch_type='mag', normalize=False) 
+
+    # Plot PSD for condition 2
+    fig_cond2_psd = epochs_cond2.plot_psd_topomap(ch_type='mag', normalize=False)
+
+    # Plot PSD for both conditions
+    fig_concat_psd = epochs_concat.plot_psd_topomap(ch_type='mag', normalize=False)
+
+    return fig_cond1_psd, fig_cond2_psd, fig_concat_psd
+
+if __name__ == "__main__" :
+
+    # Select subjects and runs and stage
+    subj_list = ["01", "02"]
+    run_list = ["07"]
+    stage = "epo"
+
+    # Select what conditions to compute (str)
+    cond1 = "LaughReal"
+    cond2 = "LaughPosed"
+    picks = "meg" # Select MEG channels
+    event_id = {'LaughReal' : 11, 'LaughPosed' : 12}
+
+# Need to check if ave is = to this process
+epochs_cond1, epochs_cond2, epochs_concat = ave_epochs(PREPROC_PATH, subj_list, run_list, cond1, cond2, "epo")
+
+fig_cond1_psd, fig_cond2_psd, fig_concat_psd = plot_PSD(epochs_cond1, epochs_cond2, epochs_concat)
