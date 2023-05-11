580c5571e213d41a9f6a9f00a2890a5ac2716847
claraElk <clara.elkhantour@gmail.com>
2023-05-10 21:15:37 -0400

    Add comments

--- test/cluster_psd.py
+++ test/cluster_psd.py
@@ -15,23 +15,27 @@ from mne.stats import permutation_cluster_1samp_test,  permutation_cluster_test
 all_evokeds_list = []
 cond1 = 'LaughReal'
 cond2 = 'LaughPosed'
+task = 'LaughterActive'
 
 contrasts_all_subject = []
 
-for subj in SUBJ_CLEAN:
-    print("=> Process task :", task, 'subject', subj)
 
-    path = '/home/claraelk/scratch/laughter_data/results/meg/reports/sub-all/erp/'
+print("--> Process task :", task)
 
-    epo_name1, _ = get_bids_file(PREPROC_PATH, stage='erp', task=task, subj=subj, condition=cond1)
-    epochs_cond1 = mne.read_epochs(path + epo_name1)
-    epochs_cond1.pick_types(meg=True, ref_meg = False,  exclude='bads')
-    
-    epo_name2, _ = get_bids_file(PREPROC_PATH, stage='erp', task=task, subj=subj, condition=cond2)
-    epochs_cond2 = mne.read_epochs(path + epo_name2)
-    epochs_cond2.pick_types(meg=True, ref_meg = False,  exclude='bads')
+path = '/home/claraelk/scratch/laughter_data/results/meg/reports/sub-all/erp/'
+
+print('--> Read epochs condition 1')
+epo_name1, _ = get_bids_file(PREPROC_PATH, stage='erp', task=task, subj=subj, condition=cond1)
+epochs_cond1 = mne.read_epochs(path + epo_name1)
+epochs_cond1.pick_types(meg=True, ref_meg = False,  exclude='bads')
+
+print('--> Read epochs condition 2')
+epo_name2, _ = get_bids_file(PREPROC_PATH, stage='erp', task=task, subj=subj, condition=cond2)
+epochs_cond2 = mne.read_epochs(path + epo_name2)
+epochs_cond2.pick_types(meg=True, ref_meg = False,  exclude='bads')
 
 # Compute freqs from 2 - 60 Hz
+print('--> Start TFR morlet')
 freqs = np.logspace(*np.log10([2, 60]))
 n_cycles = freqs / 2.  # different number of cycle per frequency
 power_cond1 = tfr_morlet(epochs_cond1, freqs=freqs, n_cycles=n_cycles, use_fft=False,
@@ -40,6 +44,7 @@ power_cond1 = tfr_morlet(epochs_cond1, freqs=freqs, n_cycles=n_cycles, use_fft=F
 power_cond2 = tfr_morlet(epochs_cond2, freqs=freqs, n_cycles=n_cycles, use_fft=False,
                         return_itc=False, decim=3, n_jobs=None, average=False)
 
+print('--> TFR morlet Done')
 
 power_cond1.apply_baseline(mode='ratio', baseline=(None, 0))
 power_cond2.apply_baseline(mode='ratio', baseline=(None, 0))
