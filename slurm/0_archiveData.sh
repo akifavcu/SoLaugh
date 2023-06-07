3081815c3f8c64cba3fbd8cc3946538ba7598afc
claraElk <clara.elkhantour@gmail.com>
2023-06-07 18:24:46 -0400

    Add archive slurm

---
+++ slurm/0_archiveData.sh
@@ -0,0 +1,7 @@
+#SBATCH --account=def-kjerbi
+#SBATCH --time=12:00:00     
+#SBATCH --mem=16  
+#SBATCH --output=S-subtrain-%j.out
+#SBATCH --error=S-subtrain-%j.err
+
+tar --create --xz /home/claraelk/nearline/def-kjerbi/claraelk/laughter_data/bids_data.tar.xz /home/claraelk/scratch/laughter_data/bids_data/
