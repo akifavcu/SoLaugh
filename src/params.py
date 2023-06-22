7205c4ff161b200dd99faf31bda8da5a02730a03
claraElk <clara.elkhantour@gmail.com>
2023-06-22 16:43:47 -0400

    Change raw path and add event bids

--- src/params.py
+++ src/params.py
@@ -2,10 +2,10 @@
 import os
 
 # Paths 
-DISK_PATH = "/run/media/clara_elk/Clara_Seagate/SoLaugh_AL/0_RAW_DATA/"
+DISK_PATH = "/home/claraelk/scratch/laughter_data/0_RAW_DATA/"
 BIDS_PATH = "/home/claraelk/scratch/laughter_data/bids_data/"
-RESULT_PATH = "/home/claraelk/scratch/laughter_data/results/"
 PREPROC_PATH = "/home/claraelk/scratch/laughter_data/preproc/"
+RESULT_PATH = "/home/claraelk/scratch/laughter_data/results/"
 PERF_PATH = "/run/media/clara_elk/Clara_Seagate/SoLaugh_MSc/PERF/solaugh_task/results_active/"
 BEHAV_PATH = RESULT_PATH + "discrimination/behavior"
 FIG_PATH = RESULT_PATH + "meg/reports/figures/"
@@ -23,6 +23,10 @@ RUN_LIST = [str(i).zfill(2) for i in range(1, 13)]
 PASSIVE_RUN = RUN_LIST[1:5] # Select runs 02 to 05
 ACTIVE_RUN = RUN_LIST[6:13] # Select runs 07 to 12
 RS_RUN = ['01', '06'] # Resting state
+
+EVENTS_ID_BIDS = {'LaughReal' : 11, 'LaughPosed' : 12, 'Good' : 99, 'Bad' : 66, 
+            'OffSet' : 5, 'EnvReal' : 21, 'ScraReal' : 31, 'EnvPosed' : 22,
+            'ScraPosed' : 32, 'Start' : 68}
 
 EVENTS_ID = {'LaughReal' : 11, 'LaughPosed' : 12, 'Good' : 99, 'Miss' : 66, 
             'OffSet' : 5, 'EnvReal' : 21, 'ScraReal' : 31, 'EnvPosed' : 22,
