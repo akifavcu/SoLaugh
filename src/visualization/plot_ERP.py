5aec03b927a5d1b6ba0bb8f18f75279124582ac5
claraElk <clara.elkhantour@gmail.com>
2023-08-09 17:10:11 -0400

    Modify colors

--- src/visualization/plot_ERP.py
+++ src/visualization/plot_ERP.py
@@ -4,6 +4,7 @@ import scipy.stats
 import pickle
 import argparse
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
 from scipy.stats import sem
 from src.utils import get_bids_file
@@ -72,7 +73,7 @@ def plot_gfp(condition1, condition2, cond1, cond2, task, picks) :
     
 
 
-def visualize_cluster(contrast, cluster_stats, event_id, task, conditions, cond1, cond2) :
+def visualize_cluster(contrast, cluster_stats, task, conditions, cond1, cond2) :
     """
     Code adapted from : 
     https://mne.tools/stable/auto_tutorials/stats-sensor-space/75_cluster_ftest_spatiotemporal.html
@@ -83,8 +84,6 @@ def visualize_cluster(contrast, cluster_stats, event_id, task, conditions, cond1
 
     for subj in SUBJ_CLEAN :
         print("processing -->", subj)
-        #_, path_evoked = get_bids_file(PREPROC_PATH, task=task, subj=subj, stage="ave")
-        #evoked = mne.read_evokeds(path_evoked, verbose=None)
         
         _, path_epochs = get_bids_file(RESULT_PATH, task=task, subj=subj, stage="AR_epo")
         epochs_AR = mne.read_epochs(path_epochs, verbose=None)
@@ -97,23 +96,31 @@ def visualize_cluster(contrast, cluster_stats, event_id, task, conditions, cond1
     # Prepare data for plotting
     evokeds = {cond1 : evoked_condition1, cond2 : evoked_condition2}
 
-    #contrast.pick_types(meg=True, ref_meg = False,  exclude='bads')
+    """
+    Code adapted from : 
+    https://mne.tools/stable/auto_tutorials/stats-sensor-space/75_cluster_ftest_spatiotemporal.html
+    """
     T_obs, clusters, p_values, _ = cluster_stats
-    
+
     p_accept = 0.01
     good_cluster_inds = np.where(p_values < p_accept)[0]
 
     # configure variables for visualization
-    colors = "crimson",'steelblue'
+    colors = "r",'steelblue'
     linestyles = '-', '--'
 
+    times = contrast.times * 1e3
 
     # loop over clusters
     for i_clu, clu_idx in enumerate(good_cluster_inds):
+        
         # unpack cluster information, get unique indices
         time_inds, space_inds = np.squeeze(clusters[clu_idx])
         ch_inds = np.unique(space_inds)
         time_inds = np.unique(time_inds)
+        
+
+        sig_times = contrast.times[time_inds]
 
         # get topography for F stat
         t_map = T_obs[time_inds, ...].mean(axis=0)
@@ -126,13 +133,15 @@ def visualize_cluster(contrast, cluster_stats, event_id, task, conditions, cond1
         mask[ch_inds, :] = True
 
         # initialize figure
-        fig, ax_topo = plt.subplots(1, 1, figsize=(10, 3))
+        fig, ax_topo = plt.subplots(1, 1, figsize=(17, 5))
 
         # plot average test statistic and mark significant sensors
         t_evoked = mne.EvokedArray(t_map[:, np.newaxis], contrast.info, tmin=0)
-        t_evoked.plot_topomap(times=0, mask=mask, axes=ax_topo, cmap='Reds',
-                              vlim=(np.min, np.max), show=False,
-                              colorbar=False, mask_params=dict(markersize=10))
+
+        t_evoked.plot_topomap(times=0, mask=mask, axes=ax_topo, cmap='bwr',
+                            cnorm = matplotlib.colors.CenteredNorm(vcenter=0), show=False,
+                            colorbar=False, mask_params=dict(markersize=10), extrapolate='head',
+                            sphere=(0, 0.01, 0, 0.184))
 
         image = ax_topo.images[0]
 
@@ -141,7 +150,7 @@ def visualize_cluster(contrast, cluster_stats, event_id, task, conditions, cond1
 
         # create additional axes (for ERF and colorbar)
         divider = make_axes_locatable(ax_topo)
-
+        
         # add axes for colorbar
         ax_colorbar = divider.append_axes('right', size='5%', pad=0.05)
         plt.colorbar(image, cax=ax_colorbar)
@@ -152,23 +161,20 @@ def visualize_cluster(contrast, cluster_stats, event_id, task, conditions, cond1
         ax_signals = divider.append_axes('right', size='300%', pad=1.2)
 
         # TODO add color for noise around signal
-        title = 'Cluster #{0}, {1} sensor(s) (p < {2})'.format(i_clu + 1, len(ch_inds), p_values[clu_idx])
-        if len(ch_inds) > 1:
-            title += "(mean)"
-
+        title = 'Cluster #{0}, {1} sensors (p={2})'.format(i_clu + 1, len(ch_inds), p_values[clu_idx])
+        
         plot_compare_evokeds(evokeds, title=title, picks=ch_inds, axes=ax_signals,
-                         colors=colors, show=False,
-                         split_legend=True, truncate_yaxis='auto')
+                        colors=colors, show=False,
+                        split_legend=True, truncate_yaxis='auto', combine="mean")
 
         # plot temporal cluster extent
         ymin, ymax = ax_signals.get_ylim()
         ax_signals.fill_betweenx((ymin, ymax), sig_times[0], sig_times[-1],
-                             color='orange', alpha=0.3)
+                            color='orange', alpha=0.3)
         # clean up viz
         mne.viz.tight_layout(fig=fig)
         fig.subplots_adjust(bottom=.05)
-        plt.savefig(FIG_PATH + 'erp/sub-all_run-all_task-{}_cond-{}_meas-Ttest-cluster_erp{}.png'.format(task, conditions, i_clu))
-
+        plt.savefig(FIG_PATH + 'clusters-erp/sub-all_run-all_task-{}_cond-{}_meas-Ttest-cluster_erp{}.png'.format(task, conditions, i_clu))
 if __name__ == "__main__" :
 
     # Conditions and task to compute
@@ -180,13 +186,6 @@ if __name__ == "__main__" :
     condition_list = [cond1, cond2]
     event_id = dict()
     picks = "meg" # Select MEG channels
-
-    for ev in EVENTS_ID :
-        for conds in condition_list :
-            if conds not in EVENTS_ID :
-                raise Exception("Condition is not an event")
-            if conds == ev :
-                event_id[conds] = EVENTS_ID[ev]
 
     print("=> Process task :", task, "for conditions :", cond1, "&", cond2)
 
@@ -206,4 +205,4 @@ if __name__ == "__main__" :
     # plot_gfp(condition1, condition2, cond1, cond2, task, picks)
 
     # Visualization of ERP clusters
-    visualize_cluster(contrast, cluster_stats, event_id, task, conditions, cond1, cond2)
+    visualize_cluster(contrast, cluster_stats, task, conditions, cond1, cond2)
