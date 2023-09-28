45a1f08c91162ea0a6081dedb5665d35ead1a159
claraElk <clara.elkhantour@gmail.com>
2023-09-28 17:21:01 -0400

    Add RandomSearch and arrange functions

--- src/models/classification_single_feature.py
+++ src/models/classification_single_feature.py
@@ -5,21 +5,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-from src.params import SUBJ_CLEAN, RESULT_PATH, ACTIVE_RUN, PASSIVE_RUN, FIG_PATH, PREPROC_PATH, FREQS_LIST
+from src.params import SUBJ_CLEAN, RESULT_PATH, ACTIVE_RUN, PASSIVE_RUN, FIG_PATH, PREPROC_PATH, FREQS_LIST, FREQS_NAMES
 from src.utils import get_bids_file
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import cross_val_score, ShuffleSplit, GroupShuffleSplit
+from sklearn.model_selection import cross_val_score, ShuffleSplit, RandomizedSearchCV, GridSearchCV, LeaveOneGroupOut
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn import svm
 
-from mne.decoding import (
-    SlidingEstimator,
-    cross_val_multiscore,
-)
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -64,91 +59,104 @@ parser.add_argument(
     type=float,
     help="Tmin to compute",
 )
+
+parser.add_argument(
+    "-compute_hilbert",
+    "--compute_hilbert",
+    default=True,
+    type=bool,
+    help="Wwither or not computing Hilbert",
+)
 args = parser.parse_args()
 
-def prepare_data(subj_list, run_list, freq_name, cond1, cond2, classifier, clf_name) : 
-
-    epochs_list = []
-
-    y_run_cond1 = []
-    y_run_cond2 = []
-
-    X_subj = []
-    y_subj = []
-
-    for subj in ['01']: 
-
-        print(subj)
-        
-        for run in ['07'] :
-            
-            # importer les psd (enveloppe spectrale)
-            _, path_epochs = get_bids_file(RESULT_PATH, task=task, subj=subj, run=run, measure=freq_name, stage="psd_epo")
-            epochs = mne.read_epochs(path_epochs, verbose=None)
-            
-            epochs.apply_baseline(baseline=(None, 0))
-            epochs.resample(50)
-
-            if subj == subj_list[0] and run == run_list[0]:
-                head_info = epochs.info['dev_head_t']
-            else:
-                epochs.info['dev_head_t'] = head_info
-                
-            epochs_list.append(epochs)
-            
-            del epochs
-            
-        epochs_all_run = mne.concatenate_epochs(epochs_list)  
-        
-        # Compute power (= envelop **2)
-        power_cond1 = epochs_all_run[cond1].get_data()**2 # Shape n_epochs, n_chan, n_times
-        power_cond2 = epochs_all_run[cond2].get_data()**2 # Shape n_epochs, n_chan, n_times
-
-        X_subj.append(np.concatenate((power_cond1, power_cond2)))
-        y_subj.append(np.concatenate((epochs_all_run[cond1].events[:, 2], epochs_all_run[cond2].events[:, 2])))
-
-    X = X_subj[-1] #take the last concatenation
-    y = y_subj[-1]
-
-    print('Shape X', X.shape)
-    print('Shape y', y.shape)
-
-    decoding_time(X, y, classifier, cond1, cond2, clf_name, epochs=epochs_all_run, sfs=True)
-
-    return X, y 
-
-def decoding_time(X, y, classifier, cond1, cond2, clf_name, epochs, sfs=True) : 
-
+def save_data(save, tmin, tmax, cond1, cond2, X_subj, y_subj, X_cond1, X_cond2, group) : 
+    sub_name = 'all'
+    run_name = 'all'
+    tmin_name = str(int(tmin*1000))
+    tmax_name = str(int(tmax*1000))
+    stage = 'ml-'+ tmin_name + '-' + tmax_name
     conditions = cond1 + '-' + cond2
 
-    # TODO : put savepoint 
-    # We will train the classifier on all left visual vs auditory trials on MEG
-    sfs = SequentialFeatureSelector(classifier, n_features_to_select=5)
+    save_X = RESULT_PATH + 'meg/reports/sub-all/ml/sub-{}_task-{}_run-{}_cond-{}_meas-{}_{}.pkl'.format(sub_name, task, 
+                                                                                                        run_name, conditions,
+                                                                                                        'X-subj', stage)
+    save_y = RESULT_PATH + 'meg/reports/sub-all/ml/sub-{}_task-{}_run-{}_cond-{}_meas-{}_{}.pkl'.format(sub_name, task, 
+                                                                                                        run_name, conditions,
+                                                                                                        'y-subj', stage)
 
-    clf = make_pipeline(StandardScaler(), sfs, classifier)
+    save_group = RESULT_PATH + 'meg/reports/sub-all/ml/sub-{}_task-{}_run-{}_cond-{}_meas-{}_{}.pkl'.format(sub_name, task, 
+                                                                                                            run_name, conditions,
+                                                                                                            'group', stage)
 
-    time_decod = SlidingEstimator(clf, scoring="roc_auc", verbose=True, n_jobs=-1)
-    # here we use cv=3 just for speed
-    scores = cross_val_multiscore(time_decod, X, y, cv=3, n_jobs=-1)
+    save_X1 = RESULT_PATH + 'meg/reports/sub-all/ml/sub-{}_task-{}_run-{}_cond-{}_meas-{}_{}.pkl'.format(sub_name, task, 
+                                                                                                        run_name, conditions,
+                                                                                                        'X-cond1', stage)
 
-    # Mean scores across cross-validation splits
-    scores = np.mean(scores, axis=0)
+    save_X2 = RESULT_PATH + 'meg/reports/sub-all/ml/sub-{}_task-{}_run-{}_cond-{}_meas-{}_{}.pkl'.format(sub_name, task, 
+                                                                                                        run_name, conditions,
+                                                                                                        'X-cond2', stage)
+    with open(save_X, 'wb') as f:
+        pickle.dump(X_subj, f)
 
-    # Plot
-    fig, ax = plt.subplots()
+    with open(save_y, 'wb') as f:
+        pickle.dump(y_subj, f)
 
-    ax.plot(epochs.times, scores, label="score")
-    ax.axhline(0.5, color="k", linestyle="--", label="chance")
-    ax.set_xlabel("Times")
-    ax.set_ylabel("AUC")  # Area Under the Curve
-    ax.legend()
-    ax.axvline(0.0, color="k", linestyle="-")
-    ax.set_title("Sensor space decoding")
+    with open(save_group, 'wb') as f:
+        pickle.dump(group, f)
 
-    plt.savefig(FIG_PATH + 'ml/sub-01_task-{}_run-all_cond-{}_meas-decoding-time_{}-5_features-.png'.format(task, conditions, clf_name))
+    with open(save_X1, 'wb') as f:
+        pickle.dump(X_cond1, f)
 
+    with open(save_X2, 'wb') as f:
+        pickle.dump(X_cond2, f)
 
-def time_window_hilbert(subj_list, RUN_LIST, tmin, tmax, conditions, FREQS_NAMES, FREQS_LIST) :
+def upload_data(tmin, tmax, cond1, cond2) : 
+    sub_name = 'all'
+    run_name = 'all'
+    tmin_name = str(int(tmin*1000))
+    tmax_name = str(int(tmax*1000))
+    stage = 'ml-'+ tmin_name + '-' + tmax_name
+    conditions = cond1 + '-' + cond2
+
+    save_X = RESULT_PATH + 'meg/reports/sub-all/ml/sub-{}_task-{}_run-{}_cond-{}_meas-{}_{}.pkl'.format(sub_name, task, 
+                                                                                                        run_name, conditions,
+                                                                                                        'X-subj', stage)
+    save_y = RESULT_PATH + 'meg/reports/sub-all/ml/sub-{}_task-{}_run-{}_cond-{}_meas-{}_{}.pkl'.format(sub_name, task, 
+                                                                                                        run_name, conditions,
+                                                                                                        'y-subj', stage)
+
+    save_group = RESULT_PATH + 'meg/reports/sub-all/ml/sub-{}_task-{}_run-{}_cond-{}_meas-{}_{}.pkl'.format(sub_name, task, 
+                                                                                                            run_name, conditions,
+                                                                                                            'group', stage)
+
+    save_X1 = RESULT_PATH + 'meg/reports/sub-all/ml/sub-{}_task-{}_run-{}_cond-{}_meas-{}_{}.pkl'.format(sub_name, task, 
+                                                                                                        run_name, conditions,
+                                                                                                        'X-cond1', stage)
+
+    save_X2 = RESULT_PATH + 'meg/reports/sub-all/ml/sub-{}_task-{}_run-{}_cond-{}_meas-{}_{}.pkl'.format(sub_name, task, 
+                                                                                                        run_name, conditions,
+                                                                                                        'X-cond2', stage)
+
+    with open(save_X, 'rb') as f:
+        X_subj = pickle.load(f)
+        
+    with open(save_y, 'rb') as f:
+        y_subj = pickle.load(f)
+        
+    with open(save_group, 'rb') as f:
+        group = pickle.load(f)
+        
+    with open(save_X1, 'rb') as f:
+        X_cond1 = pickle.load(f)
+
+    with open(save_X2, 'rb') as f:
+        X_cond2 = pickle.load(f)
+
+    arr_scores = classif_single_chan(X_subj, y_subj, group, tmin, tmax)
+
+    return X_subj, y_subj, X_cond1, X_cond2
+
+def time_window_hilbert(subj_list, RUN_LIST, tmin, tmax, cond1, cond2, FREQS_NAMES, FREQS_LIST, save=True) :
     
     print('Time window : ' + str(tmin) + '-' + str(tmax))
     epochs_list = []
@@ -158,12 +166,17 @@ def time_window_hilbert(subj_list, RUN_LIST, tmin, tmax, conditions, FREQS_NAMES
 
     X_subj = []
     y_subj = []
+
+    X_cond1 = []
+    X_cond2 = []
     
     power_list = []
     FREQS = [x for x in range(len(FREQS_LIST))]
     idx = 0 #TODO : Change that
     
     group = []
+
+    conditions = cond1 + '-' + cond2
 
     for l_freq, h_freq in FREQS_LIST :
         print('Processing freqs -->', l_freq, h_freq)
@@ -262,6 +275,9 @@ def time_window_hilbert(subj_list, RUN_LIST, tmin, tmax, conditions, FREQS_NAMES
         ave_time_cond1 = np.mean(epochs_all_run[cond1].get_data(), axis=2)
         ave_time_cond2 = np.mean(epochs_all_run[cond2].get_data(), axis=2)
         
+        X_cond1.append(ave_time_cond1)
+        X_cond2.append(ave_time_cond2)
+        
         print('Concatenation')
         X_subj.append(np.concatenate((ave_time_cond1, ave_time_cond2)))
         y_subj.append(np.concatenate((epochs_all_run[cond1].events[:, 2], epochs_all_run[cond2].events[:, 2])))
@@ -273,47 +289,20 @@ def time_window_hilbert(subj_list, RUN_LIST, tmin, tmax, conditions, FREQS_NAMES
         print(y_subj[-1].shape)
         print(np.array(group).shape)
         
-        sub_name = 'all'
-        run_name = 'all'
-        tmin_name = str(int(tmin*1000))
-        tmax_name = str(int(tmax*1000))
-        stage = 'ml-'+ tmin_name + '-' + tmax_name
 
-        save_X = RESULT_PATH + 'meg/reports/sub-all/ml/sub-{}_task-{}_run-{}_cond-{}_meas-{}_{}.pkl'.format(sub_name, task, 
-                                                                                    run_name, conditions,
-                                                                                    'X-subj', stage)
-        
-        save_y = RESULT_PATH + 'meg/reports/sub-all/ml/sub-{}_task-{}_run-{}_cond-{}_meas-{}_{}.pkl'.format(sub_name, task, 
-                                                                                    run_name, conditions,
-                                                                                    'y-subj', stage)
-        save_group = RESULT_PATH + 'meg/reports/sub-all/ml/sub-{}_task-{}_run-{}_cond-{}_meas-{}_{}.pkl'.format(sub_name, task, 
-                                                                                    run_name, conditions,
-                                                                                    'group', stage)
-        with open(save_X, 'wb') as f:
-            pickle.dump(X_subj, f)
-    
-        with open(save_y, 'wb') as f:
-            pickle.dump(y_subj, f)
+        if save == True : 
+            save_data(save, tmin, tmax, cond1, cond2, X_subj, y_subj, X_cond1, X_cond2, group)
 
-        with open(save_group, 'wb') as f:
-            pickle.dump(group, f)
-
-
-        arr_scores = classif_single_chan(X_subj, y_subj, group) 
+        arr_scores = classif_single_chan(X_subj, y_subj, group, tmin, tmax) 
 
     return X_subj, y_subj
 
-def classif_single_chan(X_subj, y_subj, group) : 
+def classif_single_chan(X_subj, y_subj, group, tmin, tmax) : 
 
     CHAN = np.arange(0, 270, 1)
 
-    scores_chan = []
+    all_scores = []
 
-    # Select the classifier & cross-val method
-    clf = svm.SVC(kernel='linear', C=1, random_state=42)
-
-    cv = GroupShuffleSplit(n_splits=5, test_size=0.3, random_state=0)
-        
     X = X_subj[-1] #X_subj[-1]
     print(X.shape)
 
@@ -322,45 +311,72 @@ def classif_single_chan(X_subj, y_subj, group) :
 
     groups = np.array(group)
 
-    X_random = np.random.rand(3240, 10)
-
-
     for chan in CHAN :
-        print('Process channel :', chan)
-
+        print('-->Process channel :', chan)
+        
+        y_pred = []
+        
+        parameters = {'kernel':('linear', 'rbf',  'rbf', 'sigmoid'), 'C':[1, 10]}
+        clf = svm.SVC()
+        
+        cv = LeaveOneGroupOut() # Cross-validation via LOGO
+        
         # Select channel of interest
         X_chan = X[:, chan]
         X_classif = X_chan.reshape(-1, 1) # Reshape (n_event, 1)
-        print(X_chan.shape)
         print(X_classif.shape)
         
-        scores = cross_val_score(clf, X=X_classif, y=y, groups=groups, cv=cv)
-        print(scores)
+        # Find best params with GridSearch
+        # Use RandomSearch
+        print('---->Find best params')
+        search = RandomizedSearchCV(clf, parameters, cv=cv, verbose=True, n_jobs=-1).fit(X=X_classif, 
+                                                                                        y=y, 
+                                                                                        groups=groups)
+            
+        # Select best params
+        best_params = search.best_estimator_
+        print('Best params : ' + str(best_params))
+
+        # Accuracy score
+        scores = search.score(X=X_classif, y=y) 
+        print('Scores', scores)
+        #print('Best scores', scores.best_score_)
         
-        # Mean scores across cross-validation splits
-        scores_mean = np.mean(scores, axis=0)
-        print('Score :', scores_mean)
+        all_scores.append(scores)
 
-        # Put results into list
-        scores_chan.append(scores_mean)
+    sub_name = 'all'
+    run_name = 'all'
+    tmin_name = str(int(tmin*1000))
+    tmax_name = str(int(tmax*1000))
+    stage = 'ml-'+ tmin_name + '-' + tmax_name
+    conditions = cond1 + '-' + cond2
 
-    # Put all scores into numpy
-    arr_scores = np.array(scores_chan)
+    save_scores = RESULT_PATH + 'meg/reports/sub-all/ml/results_single_feat/sub-{}_task-{}_run-{}_cond-{}_meas-{}_{}.pkl'.format(sub_name, task, 
+                                                                                                                                run_name, conditions,
+                                                                                                                                'scores', stage)
+    with open(save_scores, 'wb') as f:
+        pickle.dump(all_scores, f)
     
-
-    return arr_scores
+    return all_scores
     
 if __name__ == "__main__" :
 
     subj_list = SUBJ_CLEAN
     task = args.task
+
+    # Condition 1 and 2
     cond1 = args.condition1
     cond2 = args.condition2
-    freq_name = args.freq
-    conditions = cond1 + '-' + cond2
-
+    
+    # Frequence selected
+    freq_name = args.freq    
+    
+    # Time selection
     tmin = args.tmin
     tmax = args.tmax
+
+    # Weither or not to compute hilbert or upload data
+    compute_hilbert = args.compute_hilbert
 
     if task == 'LaughterActive' :
         run_list = ACTIVE_RUN
@@ -369,26 +385,24 @@ if __name__ == "__main__" :
 
     # Select what conditions to compute
     condition_list = [cond1, cond2]
+    
+    frequency_list = []
+    for i, freq in enumerate(FREQS_NAMES) : 
+        if freq == freq_name :
+            frequency_list.append(FREQS_LIST[i])
 
-    classifier = LogisticRegression(solver="liblinear")
-    clf_name = 'LR'
-
-    hilbert_window = True
-    time_decoding = False
-
-    if time_decoding == True : 
-        X, y = prepare_data(subj_list, run_list, 
-                            freq_name, 
-                            cond1, cond2, 
-                            classifier, clf_name)
-
-    elif hilbert_window == True : 
+    if compute_hilbert == True : 
         X_subj, y_subj, group, epochs_all_run = time_window_hilbert(subj_list, run_list, 
                                                                     tmin, tmax, 
                                                                     cond1, cond2,
                                                                     FREQS_NAMES=[freq_name], 
-                                                                    FREQS_LIST=[FREQS_LIST[2]], 
+                                                                    FREQS_LIST=frequency_list, 
                                                                     save=True)
+    else :
+        upload=True
+        upload_data(tmin, tmax, cond1, cond2)
+
+
 
 
     
